29cbcf765861030a0344542bbbc2d03f19eb1a6c
CarterSprague <carter.sprague@lyon.edu>
2026-03-22 10:23:26 -0500

    Created the quiz with Bootstraps and HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 console.log("script.js connected!");
+console.log("script.js connected!");
+
+const questionBlocks = document.querySelectorAll(".question-block");
+const resultBtn = document.getElementById("result-btn");
+const resultContainer = document.getElementById("result-container");
+
+const answers = {};
+
+questionBlocks.forEach((block) => {
+  const questionName = block.dataset.question;
+  const buttons = block.querySelectorAll(".answer-btn");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((btn) => btn.classList.remove("selected"));
+      button.classList.add("selected");
+      answers[questionName] = Number(button.dataset.points);
+    });
+  });
+});
+
+function displayResult() {
+  if (Object.keys(answers).length < questionBlocks.length) {
+    resultContainer.textContent = " Answer all thequestions first.";
+    return;
+  }
+
+  let total = 0;
+
+  for (let key in answers) {
+    total += answers[key];
+  }
+
+  if (total <= 4) {
+    resultContainer.textContent = "Congrats!, you are a Cat! You are a calm, cozy, and independent companion.";
+  } else {
+    resultContainer.textContent = "Congrats!, you are a Dog! You are a playful, social, and full of energy companion.";
+  }
+}
+
+resultBtn.addEventListener("click", displayResult);
